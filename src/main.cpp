fb0687f5c30e4548fc12ab3bd62df3a56aaed01b
jleroux <lerouxjoachim@gmail.com>
2023-06-02 12:46:41 +0200

    Cannot remember

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,72 @@
 #include "Msg.hpp"
 #include "Client.hpp"
 #include <iostream>
+#include <vector>
+#include <map>
+#include <sys/select.h>
+#include <sys/socket.h>
 
-//Reimplement with pointer on functions array
-void	execute(Msg &msg)
+int	error(std::string msg, int ret)
 {
-	if (msg.command == "NICK")
-		nick(msg);
-	else if (msg.command == "USER")
-		user(msg);
-	else if (msg.command == "JOIN")
-		join(msg);
-	else if (msg.command == "PRIVMSG")
-		privmsg(msg);
-	else
-		cmd_does_not_exist(msg);
+	std::cerr << msg << std::endl;
+	return (ret);
 }
 
-void	process(FILE &socket, std::string raw)
+//Init socket and bind it properly
+int	init_connection(void)
 {
-		Msg	msg;
+	int socket = socket(AF_INET, SOCK_STREAM, 0);
+	bind(socket, (const struct sockaddr *)&addr, sizeof(addr));
+	listen(socket, 3);
+	return socket;
+}
 
-		msg = parse(raw);
-
-		if (valid_msg(msg))
-			execute(msg);
-		else
-			error("Invalid msg parsed");
+void	reset_fds(fd_set &readfds, std::vector<int> sockets)
+{
+	FD_ZERO(&readfds);
+	for (int socket : sockets)
+		FD_SET(socket, &readfds);
 }
 
 int	main(void)
 {
-	std::map<std::string, Channel>	channels;
-	std::vector<Client>				clients;
-	std::vector<FILE>				sockets;
+	int								listener;
+	fd_set							readfds;
+	std::vector<int>				sockets;
+	struct timeval					timeout;
+	//std::map<std::string, Channel>	channels;
 
-	/*
-	 *
-	 * Init server here
-	 *
-	 */
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0;
 
-	sockets[0] = listner; //Need to listen for new connections too!
-	for (socket : sockets)
+	listener = init_connection();
+	while (1)
 	{
-		if (ISSET(socket.socket)) //Read only socket ready to be read
-			if (socket == listner)
-				new_client();
-			else
-				process(socket, getMsg()); //need to check what recv returns
+		reset_fds(readfds, sockets);
+
+		if (select((sockets.size() + 1), &readfds, NULL, NULL, &timeout) < 0)
+			return error("Error: select", -1);
+
+		for (int socket : sockets)
+		{
+			if (FD_ISSET(socket, &readfds))
+			{
+				if (socket == listener)
+					sockets.push_back(init_connection());
+				else
+					process(socket, getMsg());
+			}
+		}
 	}
+	return (0);
 }
+
+do
+{
+	new_sd = accept(listen_sd, NULL, NULL);
+	printf("  New incoming connection - %d\n", new_sd);
+	FD_SET(new_sd, &master_set);
+	if (new_sd > max_sd)
+		max_sd = new_sd;
+
+} while (new_sd != -1);
