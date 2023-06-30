fb70bb27bb5dfefa44a975cb69c0e5ad9179a7a5
Joachim Leroux <lerouxjoachim@gmail.com>
2023-06-30 14:07:02 +0200

    Move cmd exec into server class

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -8,14 +8,8 @@
 # include <string>
 # include <map>
 
-class Client;
-
-typedef void (Client::*CmdFunc)(Message message);
-
 class Client {
 	private:
-		static std::map<std::string, CmdFunc>	createMap(void);
-		void	nick(Message message);
 		int											_sd;
 		bool										_status;
 		std::string									_nickname;
@@ -25,7 +19,6 @@ class Client {
 		char										_recvBuffer[512];
 		std::string									_recvString;
 		Message										_recvMessage;
-		static const std::map<std::string, CmdFunc>	_cmdMap;
 
 	public:
 		Client(int sd = -1);
@@ -37,8 +30,8 @@ class Client {
 		void	sendPackets(void);
 		void	setStatus(bool status);
 		bool	getStatus(void) const;
+		Message	getMessage(void) const;
 		void	parse(void);
-		void	execute(void);
 };
 
 #endif
