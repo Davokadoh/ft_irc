393855165667ad81500cc09ca4875094e5dcce9e
Joachim Leroux <lerouxjoachim@gmail.com>
2023-07-07 11:54:26 +0200

    Check mode & add relevants fields to Channel

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -8,17 +8,30 @@
 class	Channel
 {
 	private:
-		std::set<Client*>	_clients;
 		Channel(const Channel &rhs);
 		Channel	&operator=(const Channel &rhs);
+
+		std::set<Client*>	_clients;
+		std::set<Client*>	_operators;
+		bool				_inviteMode;
+		bool				_topicMode;
+		std::string			_topic;
+		std::string			_password;
+		unsigned int		_limit;
 
 	public:
 		Channel(void);
 		~Channel(void);
 
-		void addClient(Client *client);
-
-		void	printClientList(void); // <--------- just to test: greg
+		void				addClient(Client *client);
+		std::set<Client*>	getClients(void) const;
+		void				printClientList(void);
 };
 
 #endif
+
+i: Invite-only channel
+t: Restricts TOPIC command to channel operators
+k: Set/remove the channel key (password)
+o: Give/take channel operator privilege
+l: Set/remove the user limit to channel
--- src/topic.cpp
+++ src/topic.cpp
@@ -1,40 +1,42 @@
 #include "Server.hpp"
 
-
 void	Server::topic(Client &client) {
-	Message	message = client.getMessage();
-	std::vector<std::string>	parameters;
 	std::map<string, Channel*>::iterator	channelIt;
+	std::vector<std::string>				parameters;
 	std::set<Client*>						clients;
 	std::set<Client*>						operators;
+	Message									message;
 
+	message = client.getMessage();
 	parameters = message.getParameters();
-	if (parameters.size() < 2) {
+	if (parameters.size() < 1) {
 		rply(ERR_NOTENOUGHPARAM);
 		return;
 	}
 
-	channelIt = channels.find();
+	channelIt = channels.find(parameters[0]);
 	if (channelIt == channels.end()) {
 		rply(ERR_NOSUCHCHANNEL);
 		return;
 	}
 
 	clients = channelIt->second.getClients();
-
-	if (clients.find(client.getName()) == clients.end()) {
+	if (clients.find(client) == clients.end()) {
 		rply(ERR_NOTONCHANNEL);
 		return;
 	}
 		
-	if (parameters.size() == 2) {
+	if (parameters.size() == 1) {
 		rply(RPL_TOPIC);
-	} else {
+		return;
+	}
+
+	if (channel.getTopicMode()) {
 		operators = channelIt->second.getOperators();
-		if (operators.find(client.getName()) == operators.end()) {
+		if (operators.find(client) == operators.end()) {
 			rply(ERR_CHANOPRIVSNEEDED);
 			return;
 		}
-		channel.setTopic(parameters[2]);
 	}
+	channelIt->second.setTopic(parameters[1]);
 }
