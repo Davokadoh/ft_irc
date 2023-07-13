cd1e659607f9e2f11e99a6097754df28bdd0d895
Grégoire Beaumann <gregoirebeaumann@MacBook-Pro-de-Gregoire.local>
2023-07-13 15:50:49 +0200

    mise en norm des macros + corr macros

--- inc/Macros.hpp
+++ inc/Macros.hpp
@@ -1,33 +1,33 @@
 #ifndef	MACROS_HPP
 # define MACROS_HPP
 
-# define RPL_WELCOME(nickname) " 001 " + nickname + " :Welcome to the Ft_irc.42 Network, " + nickname
-# define RPL_YOURHOST(nickname) " 002 " + nickname + " :Your host is Ft_irc.42"
-# define RPL_CREATED(nickname, date) " 003 " + nickname + " :This server was created " + date
-# define RPL_MYINFO(nickname, servername) " 004 " + nickname + " Ft_irc.42"
-# define RPL_ISUPPORT(nickname) " 005 " + nickname + " JE SAIS PAS" // a revoir
-# define RPL_LUSERCLIENT(nickname, nbrUsers) " 251 " + nickname + " :There are " + nbrUsers + "  users and 0 invisible on  1 servers"
-# define RPL_LUSEROP(nickname) " 252 " + nickname + " 0 :operator(s) online" // we do not handle server operator
-# define RPL_LUSERCHANNELS(nickname, nbrChannels) " 254 " + nickname + " " + nbrChannels + " :channels formed"
-# define RPL_LUSERME(nickname, nbrClients) " 255 " + nickname + " :I have " + nbrClients + " clients and 1 servers"
+# define RPL_WELCOME(client) " 001 " + client + " :Welcome to the Ft_irc.42 Network, " + client
+# define RPL_YOURHOST(client) " 002 " + client + " :Your host is Ft_irc.42"
+# define RPL_CREATED(client, date) " 003 " + client + " :This server was created " + date
+# define RPL_MYINFO(client) " 004 " + client + " Ft_irc.42"
+# define RPL_ISUPPORT(client) " 005 " + client + " JE SAIS PAS" // a revoir
+# define RPL_LUSERCLIENT(client, nbrUsers) " 251 " + client + " :There are " + nbrUsers + "  users and 0 invisible on  1 servers"
+# define RPL_LUSEROP(client) " 252 " + client + " 0 :operator(s) online" // we do not handle server operator
+# define RPL_LUSERCHANNELS(client, nbrChannels) " 254 " + client + " " + nbrChannels + " :channels formed"
+# define RPL_LUSERME(client, nbrClients) " 255 " + client + " :I have " + nbrClients + " clients and 1 servers"
 # define RPL_NOTOPIC(client, channel) " 331 " + client + " " + channel + " :No topic is set"
 # define RPL_TOPIC(client, channel, topic) " 332 " + client + " " + channel + " " + topic
-# define RPL_INVITING(nickname,channel) " 341 " + nickname + " " + channel
-# define RPL_NAMREPLY(client, channel, nick) " 353 " + client + " = " + channel + " " + nick
+# define RPL_INVITING(client, target, channel) " 341 " + client + " " + target + " " + channel
+# define RPL_NAMREPLY(client, channel, target) " 353 " + client + " = " + channel + " " + target
 # define RPL_ENDOFNAMES(client, channel) " 366 " + client + " " + channel + " :End of /NAMES list"
 # define ERR_NOSUCHCHANNEL(client, channel) " 403 " + client + " " + channel  + " :No such channel"
-# define ERR_UNKNOWNCOMMAND(nickname) " 421 " + nickname + " :Unknown command"
-# define ERR_NOMOTD(nickname) " 422 " + nickname + " :MOTD File is missing"
-# define ERR_NONICKNAMEGIVEN(nickname) " 431 " + nickname + " :No nickname given"
-# define ERR_ERRONEUSNICKNAME(nickname, newNick) " 432 " + nickname + " " + newNick + " :Erroneous Nickname"
-# define ERR_NICKNAMEINUSE(nickname, newNick) " 433 " + nickname + " " + newNick + " :Nickname already in use"
-# define ERR_USERNOTINCHANNEL(client, nickname, channel) " 441 " + client + " " + nickname + " " + channel + " :They aren't on that channel"
+# define ERR_UNKNOWNCOMMAND(client, cmd) " 421 " + client + " " + cmd + " :Unknown command"
+# define ERR_NOMOTD(client) " 422 " + client + " :MOTD File is missing"
+# define ERR_NONICKNAMEGIVEN(client) " 431 " + client + " :No nickname given"
+# define ERR_ERRONEUSNICKNAME(client, newNick) " 432 " + client + " " + newNick + " :Erroneous Nickname"
+# define ERR_NICKNAMEINUSE(client, newNick) " 433 " + client + " " + newNick + " :Nickname already in use"
+# define ERR_USERNOTINCHANNEL(client, target, channel) " 441 " + client + " " + target + " " + channel + " :They aren't on that channel"
 # define ERR_NOTONCHANNEL(client, channel) " 442 " + client + " " + channel + " :You're not on that channel"
-# define ERR_USERONCHANNEL(client, nickname, channel) " 443 " + client + " " + nickname + " " + channel + " :is already on channel"
-# define ERR_NOTREGISTERED(nickname) " 451 " + nickname + " :You have not registered"
-# define ERR_NEEDMOREPARAMS(nickname) " 461 " + nickname + " :Not enough parameters"
-# define ERR_ALREADYREGISTERED(nickname) " 462 " + nickname + " :You may not reregister"
-# define ERR_INVITEONLYCHAN(nickname, channel) " 473 " + nickname + " " + channel + " :Cannot join channel (+i)"
+# define ERR_USERONCHANNEL(client, target, channel) " 443 " + client + " " + target + " " + channel + " :is already on channel"
+# define ERR_NOTREGISTERED(client) " 451 " + client + " :You have not registered"
+# define ERR_NEEDMOREPARAMS(client, cmd) " 461 " + client + " " + cmd + " :Not enough parameters"
+# define ERR_ALREADYREGISTERED(client) " 462 " + client + " :You may not reregister"
+# define ERR_INVITEONLYCHAN(client, channel) " 473 " + client + " " + channel + " :Cannot join channel (+i)"
 # define ERR_CHANOPRIVSNEEDED(client, channel) " 482 " + client + " " + channel + " :You're not channel operator"
 
 #endif
