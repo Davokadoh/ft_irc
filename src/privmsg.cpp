32fdc4c7fb5ce4850d854694c7f84f99187ad56d
Grégoire Beaumann <gregoirebeaumann@MacBook-Pro-de-Gregoire.local>
2023-07-13 16:32:11 +0200

    1er jet des msg prive et de channel

--- src/privmsg.cpp
+++ src/privmsg.cpp
@@ -9,6 +9,7 @@ void	Server::privmsg(Client &client)
 	}
 	std::string sendTo = client.getMessage().getParameters()[0];
 	std::string message = client.getMessage().getParameters()[1];
+	std::map<std::string, Channel*>::iterator itChannel = this->_channels.find(sendTo);
 	//int			triggerUser = 0;
 	//int			triggerChannel = 0;
 
@@ -17,12 +18,13 @@ void	Server::privmsg(Client &client)
 		if (it->second->getNickname() == sendTo)
 		{
 	//		triggerUser = 1;
-			it->second->sendMessage(client.getSource() + " PRIVMSG " + client.getNickname() + " " + message);
+			it->second->sendMessage(client.getSource() + " PRIVMSG " + it->second->getNickname() + " :" + message);
 		}
 	}
 
-	if(this->_channels.find(sendTo) != this->_channels.end())
+	if(itChannel != this->_channels.end())
 	{
+		itChannel->second->sendToAll(client.getSource() + " PRIVMSG " + itChannel->second->getName() + " :" + message);
 	//	triggerChannel = 1;
 	}
 
