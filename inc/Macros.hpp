09f9de198ba592b9ef678c54aace54558436e28f
Joachim Leroux <lerouxjoachim@gmail.com>
2023-07-21 18:03:34 +0200

    Add WHO macros

--- inc/Macros.hpp
+++ inc/Macros.hpp
@@ -1,18 +1,23 @@
 #ifndef MACROS_HPP
 #define MACROS_HPP
 
-#define RPL_WELCOME(client)                         " 001 " + client + " :Welcome to the Ft_irc.42 Network, " + client
-#define RPL_YOURHOST(client)                        " 002 " + client + " :Your host is Ft_irc.42"
-#define RPL_CREATED(client, date)                   " 003 " + client + " :This server was created " + date
-#define RPL_MYINFO(client)                          " 004 " + client + " Ft_irc.42"
-#define RPL_LUSERCLIENT(client, nbrUsers)           " 251 " + client + " :There are " + nbrUsers + "  users and 0 invisible on  1 servers"
-#define RPL_LUSEROP(client)                         " 252 " + client + " 0 :operator(s) online" // we do not handle server operator
-#define RPL_LUSERCHANNELS(client, nbrChannels)      " 254 " + client + " " + nbrChannels + " :channels formed"
-#define RPL_LUSERME(client, nbrClients)             " 255 " + client + " :I have " + nbrClients + " clients and 1 servers"
-#define RPL_CHANNELMODEIS(client, channel, modes)   " 324 " + client + " " + channel + " " + modes
-#define RPL_NOTOPIC(client, channel)                " 331 " + client + " " + channel + " :No topic is set"
-#define RPL_TOPIC(client, channel, topic)           " 332 " + client + " " + channel + " " + topic
-#define RPL_INVITING(client, target, channel)       " 341 " + client + " " + target + " " + channel
+#define RPL_WELCOME(client)                       " 001 " + client + " :Welcome to the Ft_irc.42 Network, " + client
+#define RPL_YOURHOST(client)                      " 002 " + client + " :Your host is Ft_irc.42"
+#define RPL_CREATED(client, date)                 " 003 " + client + " :This server was created " + date
+#define RPL_MYINFO(client)                        " 004 " + client + " Ft_irc.42"
+#define RPL_LUSERCLIENT(client, nbrUsers)         " 251 " + client + " :There are " + nbrUsers + "  users and 0 invisible on  1 servers"
+#define RPL_LUSEROP(client)                       " 252 " + client + " 0 :operator(s) online" // we do not handle server operator
+#define RPL_LUSERCHANNELS(client, nbrChannels)    " 254 " + client + " " + nbrChannels + " :channels formed"
+#define RPL_LUSERME(client, nbrClients)           " 255 " + client + " :I have " + nbrClients + " clients and 1 servers"
+#define RPL_CHANNELMODEIS(client, channel, modes) " 324 " + client + " " + channel + " " + modes
+#define RPL_NOTOPIC(client, channel)              " 331 " + client + " " + channel + " :No topic is set"
+#define RPL_TOPIC(client, channel, topic)         " 332 " + client + " " + channel + " " + topic
+#define RPL_INVITING(client, target, channel)     " 341 " + client + " " + target + " " + channel
+
+#define RPL_WHOREPLY(client, channel, username, host, server, nick, flags, realname) \
+  " 352 " + client + " " + channel + " " + username + " " + host + " " + server + " " + nick + " " + flags + " :0 " + realname
+
+#define RPL_ENDOFWHO(client, mask)                  " 315 " + client + " " + mask + " :End of /WHO list"
 #define RPL_NAMREPLY(client, channel, target)       " 353 " + client + " = " + channel + " " + target
 #define RPL_ENDOFNAMES(client, channel)             " 366 " + client + " " + channel + " :End of /NAMES list"
 #define RPL_MOTD(client, msg)                       " 372 " + client + " :" + msg
@@ -39,7 +44,7 @@
 #define ERR_UNKNOWNMODE(client, mode)               " 472 " + client + " " + mode + " :is unknown mode char to me"
 #define ERR_INVITEONLYCHAN(client, channel)         " 473 " + client + " " + channel + " :Cannot join channel (+i)"
 #define ERR_BADCHANNELKEY(client, channel)          " 475 " + client + " " + channel + " :Cannot join channel (+k)"
-#define ERR_BADCHANMASK(channel)					" 476 " + channel + " :Bad Channel Mask"
+#define ERR_BADCHANMASK(channel)                    " 476 " + channel + " :Bad Channel Mask"
 #define ERR_CHANOPRIVSNEEDED(client, channel)       " 482 " + client + " " + channel + " :You're not channel operator"
 #define ERR_INVALIDKEY(client, channel)             " 525 " + client + " " + channel + " :Key is not well-formed"
 
